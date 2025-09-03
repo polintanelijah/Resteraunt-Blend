32336000e50b714c0c55df51fe4c330314e7035a
polintanelijah <141956598+polintanelijah@users.noreply.github.com>
2025-09-03 13:58:49 -0400

    Update get_restaurant_data.py
    Added location bias, hard coded to columbus with 30 mile radius

--- get_restaurant_data.py
+++ get_restaurant_data.py
@@ -1,5 +1,5 @@
 from google.maps import places_v1
-
+from google.type import latlng_pb2
 
 API_KEY = ""
 
@@ -7,11 +7,28 @@ def get_restaurant_data(resName):
     client = places_v1.PlacesClient(
         client_options={"api_key": API_KEY}
     )
+    
+    # Coordinates of Columbus
+    lat = 39.9625
+    lng = -83.0032
+    radius_meters = 50000.0     # ~30 mile radius
 
+    # Create location bias
+    center_point = latlng_pb2.LatLng(latitude=lat, longitude=lng)
+    circle_area = places_v1.types.Circle(
+        center=center_point,
+        radius=radius_meters
+    )
+    location_bias = places_v1.SearchTextRequest.LocationBias(
+        circle=circle_area
+    )
+
+    # Search for restaurant
     search_request = places_v1.SearchTextRequest(
         text_query=resName,
+        location_bias=location_bias
     )
-    field_mask = "places.formattedAddress,places.types,places.editorialSummary"
+    field_mask = "places.formattedAddress,places.types,places.editorialSummary" # Data wanted
     search_response = client.search_text(
         request=search_request,
         metadata=[("x-goog-fieldmask", field_mask)],
